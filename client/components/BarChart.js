b3391b23ef359ec09a51cf19d01ab214f40ec9a9
Chad Yoder <cwyoder13@gmail.com>
2020-11-12 22:39:02 -0600

    added left axis and grid

--- client/components/BarChart.js
+++ client/components/BarChart.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { scaleLinear, scaleBand, max, select, range, line } from 'd3';
+import { scaleLinear, scaleBand, max, select, range, line, axisBottom, axisLeft } from 'd3';
 import { intervalAverages } from '../utils';
 
 export default class BarChart extends React.Component {
@@ -22,25 +22,32 @@ export default class BarChart extends React.Component {
       .reverse();
     const averages = intervalAverages(dataToChart, 7);
 
-    // console.log('data:', dataToChart)
-    // console.log('averages:', averages)
+    const margin = {top: 20, right: 30, bottom: 30, left: 40};
 
     const dataMax = max(dataToChart);
     const yScale = scaleLinear()
       .domain([0, dataMax])
-      .range([0, this.props.size[1]])
+      .rangeRound([margin.top, this.props.size[1] - margin.bottom])
 
     const yScaleAvg = scaleLinear()
       .domain([0, dataMax])
-      .range([this.props.size[1], 0])
+      .rangeRound([this.props.size[1] - margin.bottom, margin.top])
 
     const xScale = scaleBand()
       .domain(range(this.props.data.length))
-      .rangeRound([0, this.props.size[0]])
+      .range([margin.left, this.props.size[0] - margin.right])
 
     const avgLine = line()
       .x((d, idx) => xScale(idx) + (xScale.bandwidth() / 2))
       .y(d => yScaleAvg(d))
+
+    select(node).append("g")
+      .attr("class", "grid")
+      .attr("transform", `translate(${margin.left},0)`)
+      .call(axisLeft(yScaleAvg).ticks(null, "s")
+        .tickSize(-this.props.size[0])
+        .tickFormat(""))
+      .call(g => g.select(".domain").remove())
 
     select(node).append('g')
         .attr("fill", "steelblue")
@@ -49,8 +56,8 @@ export default class BarChart extends React.Component {
       .data(dataToChart)
       .join("rect")
         .attr('x', (d,i) => xScale(i))
-        .attr('y', d => this.props.size[1] - yScale(d))
-        .attr('height', d => yScale(d))
+        .attr('y', d => yScaleAvg(d) )
+        .attr('height', d => yScale(d) - margin.top)
         .attr('width', xScale.bandwidth());
 
     select(node).append("path")
@@ -59,6 +66,13 @@ export default class BarChart extends React.Component {
       .attr("stroke-miterlimit", 1)
       .attr("stroke-width", 3)
       .attr("d", avgLine(averages));
+
+    select(node).append("g")
+      .attr("class", "y axis")
+      .attr("transform", `translate(${margin.left},0)`)
+      .call(axisLeft(yScaleAvg).ticks(null, "s"))
+      .call(g => g.select(".domain").remove());
+
   }
 
   render() {
